9f969077b8751726351bed50e84cd2846c8fb951
y0ng0p3 <vaillantyomen@gmail.com>
2025-08-02 12:12:44 -0400

    modules

---
+++ script/Interactions.s.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundFundMe is Script {
+    uint256 constant SEND_VALUE = 0.01 ether;
+
+    function interactWithFundMeFund(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        console.log("Funded FundMe with %s", SEND_VALUE);
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        interactWithFundMeFund(mostRecentlyDeployed);
+    }
+}
+
+contract WithdrawFundMe is Script {
+    function interactWithFundMeWithdraw(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        interactWithFundMeWithdraw(mostRecentlyDeployed);
+    }
+}
